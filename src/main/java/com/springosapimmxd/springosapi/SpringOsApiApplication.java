553da04d7f825f0d02df3a7ca854cd74f5a44403
MarceloMachadoxD <mastermanson@gmail.com>
2021-03-09 20:12:06 -0300

    first commit

---
+++ src/main/java/com/springosapimmxd/springosapi/SpringOsApiApplication.java
@@ -0,0 +1,13 @@
+package com.springosapimmxd.springosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springosapimmxd/springosapi/SpringOsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springosapimmxd.springosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
