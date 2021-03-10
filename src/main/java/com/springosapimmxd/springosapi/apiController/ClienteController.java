cefa979e0e687fce1b07758d2344e8a4f0d9d427
MarceloMachadoxD <mastermanson@gmail.com>
2021-03-10 19:50:44 -0300

    Dev tools dependecy

--- src/main/java/com/springosapimmxd/springosapi/apiController/ClienteController.java
+++ src/main/java/com/springosapimmxd/springosapi/apiController/ClienteController.java
@@ -14,7 +14,7 @@ public class ClienteController {
     public List<Cliente> listar(){
         Cliente cliente1 = new Cliente(1L,"fulano","fulano@lol.com","12345678");
         Cliente cliente2 = new Cliente(2L,"beltrano","beltrano@lol.com","98765432");
-        return Arrays.asList(cliente1,cliente2);
+        return Arrays.asList(cliente1, cliente2);
     }
 
 }
