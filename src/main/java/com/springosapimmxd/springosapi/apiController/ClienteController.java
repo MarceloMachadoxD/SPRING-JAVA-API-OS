eb66f2071a0f4e78dc2cafd35b50a2228a345ced
MarceloMachadoxD <mastermanson@gmail.com>
2021-03-10 21:32:00 -0300

    Mapeamento de entidade JPA, Criaçãode repository e ajuste da classe ClienteController

--- src/main/java/com/springosapimmxd/springosapi/apiController/ClienteController.java
+++ src/main/java/com/springosapimmxd/springosapi/apiController/ClienteController.java
@@ -1,20 +1,23 @@
 package com.springosapimmxd.springosapi.apiController;
 
 import com.springosapimmxd.springosapi.domain.model.Cliente;
+import com.springosapimmxd.springosapi.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
-    @GetMapping("/clientes")
-    public List<Cliente> listar(){
-        Cliente cliente1 = new Cliente(1L,"fulano","fulano@lol.com","12345678");
-        Cliente cliente2 = new Cliente(2L,"beltrano","beltrano@lol.com","98765432");
-        return Arrays.asList(cliente1, cliente2);
-    }
+    @Autowired
+    private ClienteRepository clienteRepository;
 
+    @GetMapping("/clientes")
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+
+    }
 }
--- src/main/java/com/springosapimmxd/springosapi/domain/model/Cliente.java
+++ src/main/java/com/springosapimmxd/springosapi/domain/model/Cliente.java
@@ -1,7 +1,17 @@
 package com.springosapimmxd.springosapi.domain.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
@@ -48,4 +58,21 @@ public class Cliente {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+
+
 }
