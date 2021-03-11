9f4c01558c240c594e6fc4fd1a10702f38ee6fb5
MarceloMachadoxD <mastermanson@gmail.com>
2021-03-11 19:33:14 -0300

    GET mapping no ClienteController

--- src/main/java/com/springosapimmxd/springosapi/apiController/ClienteController.java
+++ src/main/java/com/springosapimmxd/springosapi/apiController/ClienteController.java
@@ -3,11 +3,14 @@ package com.springosapimmxd.springosapi.apiController;
 import com.springosapimmxd.springosapi.domain.model.Cliente;
 import com.springosapimmxd.springosapi.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ClienteController {
@@ -18,6 +21,25 @@ public class ClienteController {
     @GetMapping("/clientes")
     public List<Cliente> listar() {
         return clienteRepository.findAll();
+    }
+
+    @GetMapping("/cliente/{clienteId}")
+    public ResponseEntity<Cliente> FindById(@PathVariable Long clienteId){
+        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+        if (cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
 
     }
+
+    @GetMapping("/clientenome/{clienteNome}")
+    public ResponseEntity<Cliente> FindByNome(@PathVariable String clienteNome){
+        Optional<Cliente> cliente = clienteRepository.findByNome(clienteNome);
+        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
 }
--- src/main/java/com/springosapimmxd/springosapi/repository/ClienteRepository.java
+++ src/main/java/com/springosapimmxd/springosapi/repository/ClienteRepository.java
@@ -4,8 +4,12 @@ import com.springosapimmxd.springosapi.domain.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 
+    Optional<Cliente> findByNome(String clienteNome);
 }
