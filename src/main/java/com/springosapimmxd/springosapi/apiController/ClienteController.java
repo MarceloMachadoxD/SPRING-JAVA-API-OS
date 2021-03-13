1ab03ca01917c675451ed02dcde597e16dfbe24a
MarceloMachadoxD <mastermanson@gmail.com>
2021-03-13 17:29:30 -0300

    Post Put and Delete methods - ClienteController

--- src/main/java/com/springosapimmxd/springosapi/apiController/ClienteController.java
+++ src/main/java/com/springosapimmxd/springosapi/apiController/ClienteController.java
@@ -3,10 +3,9 @@ package com.springosapimmxd.springosapi.apiController;
 import com.springosapimmxd.springosapi.domain.model.Cliente;
 import com.springosapimmxd.springosapi.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -39,6 +38,38 @@ public class ClienteController {
     public ResponseEntity<Cliente> FindByNome(@PathVariable String clienteNome){
         Optional<Cliente> cliente = clienteRepository.findByNome(clienteNome);
         return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping("/cliente")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente){
+
+        return  clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/cliente/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente){
+      /* a ide sugere escrever assim */
+        if (clienteRepository.existsById(clienteId)) {
+            cliente.setId(clienteId);
+            cliente =  clienteRepository.save(cliente);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/cliente/{clienteId}")
+    public ResponseEntity<Cliente> Remover(@PathVariable Long clienteId){
+        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+        if (cliente.isPresent()){
+            clienteRepository.deleteById(clienteId);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
 
     }
 
--- src/main/java/com/springosapimmxd/springosapi/repository/ClienteRepository.java
+++ src/main/java/com/springosapimmxd/springosapi/repository/ClienteRepository.java
@@ -3,6 +3,7 @@ package com.springosapimmxd.springosapi.repository;
 import com.springosapimmxd.springosapi.domain.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
