4082f4c7c17f188d7f0e650042d63f49a5493ee9
MarceloMachadoxD <mastermanson@gmail.com>
2021-03-14 13:16:19 -0300

    Tratamento de exceção personalizada para erro de cadastro de Cliente

---
+++ src/main/java/com/springosapimmxd/springosapi/apiController/ExceltpionHandler/ApiExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.springosapimmxd.springosapi.apiController.ExceltpionHandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+       Problema problema = new Problema(status.value(), LocalDateTime.now(),"Um ou mais campos inválidos ou obrigatórios sem dados");
+        return super.handleExceptionInternal(ex,problema,headers,status,request);
+    }
+}
